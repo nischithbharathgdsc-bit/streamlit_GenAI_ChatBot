92715f66be52135feea0691bb053b3e9b8667464
nischithbharathgdsc-bit <nischithbharath.gdsc@gmail.com>
2025-10-13 19:46:01 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -11,7 +11,7 @@ st.set_page_config(
     page_icon="🤖",
     layout="centered",
 )
-st.title("💬 Generative AI Chatbot")
+st.title("💬 Welcome to Nischith's personal Generative AI Chatbot")
 
 
 # initiate chat history
@@ -52,3 +52,4 @@ if user_prompt:
 # get response from llm
 # save response in chat history
 # display llm response on screen
+
